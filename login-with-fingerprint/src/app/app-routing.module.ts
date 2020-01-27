5bbe87316f9b2f3182e3ce708a89df516f5cb37d
Alex Leko <lekodeveloperweb@gmail.com>
2020-01-26 22:22:57 -0300

    fingerprint page

--- login-with-fingerprint/src/app/app-routing.module.ts
+++ login-with-fingerprint/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () =>
       import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'fingerprint',
+    loadChildren: () => import('./fingerprint/fingerprint.module').then( m => m.FingerprintPageModule)
   }
 ];
 
