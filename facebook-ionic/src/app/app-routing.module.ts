019a496bdfd49ee680be38b12da7c92a25ce3dd0
Alex Leko <lekodeveloperweb@hotmail.com>
2020-08-23 14:41:12 -0300

    start facebook clone

---
+++ facebook-ionic/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "app/tabs/home",
+    pathMatch: "full",
+  },
+  {
+    path: "app",
+    loadChildren: () =>
+      import("./tabs/tabs.module").then((m) => m.TabsPageModule),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
